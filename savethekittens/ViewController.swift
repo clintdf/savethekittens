f6b8a6122cb098149e73a9247c029403c4882806
clintdf <cfelker@cox.net>
2018-04-16 12:02:54 -0400

    Initial Commit

---
+++ savethekittens/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  savethekittens
+//
+//  Created by Clint Felker on 4/16/18.
+//  Copyright © 2018 Clint Felker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
